3c72e2aee08190fd7c8661d76f0ec1cfaf31dca0
Jeremy Parker <jeremyparker@pop-os.localdomain>
2021-06-10 20:05:24 +1200

    make CPU usage for recent time

--- src/processor.cpp
+++ src/processor.cpp
@@ -16,5 +16,14 @@ float Processor::Utilization() {
         std::stol(SystemCPUJiffies[LinuxParser::kSoftIRQ_]) + 
         std::stol(SystemCPUJiffies[LinuxParser::kSteal_]);
     float total = idle + nonIdle;
+    if (previousTotal_ && previousIdle_) {
+        float currentTotal = total - previousTotal_;
+        float currentIdle = idle - previousIdle_;
+        previousTotal_ = total;
+        previousIdle_ = idle;
+        return (currentTotal - currentIdle) / currentTotal;
+    }
+    previousTotal_ = total;
+    previousIdle_ = idle;
     return (total - idle) / total; 
 }
